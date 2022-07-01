ed7f6ccdf25cca637c6a44dee49973775b87adc7
Hugo <hugosimoescontato@gmail.com>
2022-06-30 23:00:31 -0300

    Day 2

--- app/src/main/java/br/com/portfolio/seven_days_of_code/MainActivity.kt
+++ app/src/main/java/br/com/portfolio/seven_days_of_code/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.portfolio.seven_days_of_code.model.User
 import br.com.portfolio.seven_days_of_code.ui.theme.SevendaysofcodeTheme
 
@@ -27,10 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column() {
-                        ImageResource()
-                        Perfil(User("Hugo", "hugosimoess", "Dev Kotlin"))
-                    }
+                    ProfileScreen()
                 }
             }
         }
@@ -38,24 +45,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageResource() {
-    val image: Painter = painterResource(id = R.drawable.ic_launcher_foreground)
-    Image(painter = image,contentDescription = "Foto de Perfil")
-}
-
-@Composable
-fun Perfil(user: User) {
-    Column() {
-        Text(user.name)
-        Text(user.username)
-        Text(user.bio)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SevendaysofcodeTheme {
-        Perfil(User("Hugo", "hugosimoess", "Dev Kotlin"))
+fun ProfileScreen() {
+    Column {
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color(0xFF2d333b), shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                )
+                .height(boxHeight)
+        ) {
+            Image(
+                painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "profile pic",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
+            )
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Hugo Simões",
+                fontSize = 32.sp
+            )
+            Text(
+                "hugosimoess",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Text(
+            "Android Developer",
+            Modifier
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 8.dp
+                )
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
